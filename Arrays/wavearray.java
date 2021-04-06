8664a8fed8600ee94bed2f4e321e3cd3819d9009
VaradNaik <54986840+VaradNaik@users.noreply.github.com>
2021-04-07 02:45:33 +0530

    solution to wavearray

---
+++ Arrays/wavearray.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public ArrayList<Integer> wave(ArrayList<Integer> A) {
+        if(A.size() <= 1)return A;
+        /*
+        Logic - notice that int the answer,the first element is always the second smallest
+        number and the first element is the smallest number.
+        After that, the third elelment is the 4th smallest and fourth element
+        is the third smallest. So basically if you sort and then group in pairs 
+        of (second,first) upto the end of the array you get the solution.
+        Note that since you have sorted, the first element of the next pair would 
+        always be greater than the second element of the current pair
+        */
+        Collections.sort(A);
+        ArrayList<Integer> answer = new ArrayList<>();
+        int i =1;
+        int n= A.size();
+        while(i < n){
+            answer.add(A.get(i));   //greater element of the pair first
+            answer.add(A.get(i-1));
+            i += 2;
+        }
+        if(i==n){                   //to handle the case where number of elements are odd
+            answer.add(A.get(i-1));
+        }
+        return answer;
+    }
+}
